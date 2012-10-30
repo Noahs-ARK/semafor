ef7c84be39822f6a935c7fff09fac579453185fe
Sam Thomson <sam@knewton.com>
2012-10-30 16:28:51 -0400

    Pulled step 3ii of training/README out into a shell script

---
+++ training/combineAlphabets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+source "$(dirname ${0})/config"
+
+# step 3.2: combine alphabets
+${JAVA_HOME_BIN}/java -classpath ${classpath} -Xms8000m -Xmx8000m\
+  edu.cmu.cs.lti.ark.fn.identification.CombineAlphabets \
+  ${datadir} \
+  ${datadir}/alphabet_combined.dat
