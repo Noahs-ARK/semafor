a2ba8930d2b06a5fa06aae44d3cfede7af52f3f7
Sam Thomson <sam@knewton.com>
2012-11-03 23:36:58 -0400

    Slight code cleanup

--- edu/cmu/cs/lti/ark/fn/data/prep/AllAnnotationsMergingWithoutNE.java
+++ edu/cmu/cs/lti/ark/fn/data/prep/AllAnnotationsMergingWithoutNE.java
@@ -23,33 +23,38 @@ package edu.cmu.cs.lti.ark.fn.data.prep;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
 import edu.cmu.cs.lti.ark.fn.utils.LemmatizeStuff;
 
-public class AllAnnotationsMergingWithoutNE
-{
-	public static void main(String[] args)
-	{
-		ArrayList<String> tokenizedSentences = ParsePreparation.readSentencesFromFile(args[0]);
+
+public class AllAnnotationsMergingWithoutNE {
+	public static void main(String[] args) {
+		// parse args
+		final String tokenizedFile = args[0];
+		final String conllParseFile = args[1];
+		final String infile = args[2];
+		final String stopWordsFile = args[3];
+		final String wordNetConfigFile = args[4];
+		final String outfile = args[5];
+
+		ArrayList<String> tokenizedSentences = ParsePreparation.readSentencesFromFile(tokenizedFile);
 		ArrayList<String> neSentences = findDummyNESentences(tokenizedSentences);
-		ArrayList<ArrayList<String>> parses = OneLineDataCreation.readCoNLLParses(args[1]);
-		ArrayList<String> perSentenceParses=OneLineDataCreation.getPerSentenceParses(parses,tokenizedSentences,neSentences);
-		ParsePreparation.writeSentencesToTempFile(args[2], perSentenceParses);
-		LemmatizeStuff.lemmatize(args[3], args[4], args[2], args[5]);
+		ArrayList<ArrayList<String>> parses = OneLineDataCreation.readCoNLLParses(conllParseFile);
+		ArrayList<String> perSentenceParses =
+				OneLineDataCreation.getPerSentenceParses(parses, tokenizedSentences, neSentences);
+		ParsePreparation.writeSentencesToTempFile(infile, perSentenceParses);
+		LemmatizeStuff.lemmatize(stopWordsFile, wordNetConfigFile, infile, outfile);
 	}
 	
-	public static ArrayList<String> findDummyNESentences(ArrayList<String> tokenizedSentences)
-	{
+	public static ArrayList<String> findDummyNESentences(ArrayList<String> tokenizedSentences) {
 		ArrayList<String> res = new ArrayList<String>();
-		for(String sent:tokenizedSentences)
-		{
-			StringTokenizer st = new StringTokenizer(sent.trim());
+		for(String sentence : tokenizedSentences) {
+			final StringTokenizer st = new StringTokenizer(sentence.trim());
 			String resSent = "";
 			while(st.hasMoreTokens())
 			{
-				resSent+=st.nextToken()+"_O"+" ";
+				resSent += st.nextToken() + "_O ";
 			}
-			resSent=resSent.trim();
+			resSent = resSent.trim();
 			res.add(resSent);
 		}
 		return res;
