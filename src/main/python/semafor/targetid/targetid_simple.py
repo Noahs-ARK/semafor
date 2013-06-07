e4bda2c4a39459aa475f644da7c6c777db4e368d
Sam Thomson <sammthomson@gmail.com>
2013-06-07 14:01:15 -0400

    bugfix, cleanup of targetid_simple.py

--- src/main/python/semafor/targetid/targetid_simple.py
+++ src/main/python/semafor/targetid/targetid_simple.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2.7
 """
 Translation of SEMAFOR 3.0's edu.cmu.cs.lti.ark.fn.segmentation.RoteSegmenter.java into Python.
 This uses a whitelist of n-grams extracted from FrameNet gold frame annotations, 
@@ -9,38 +10,27 @@ Legacy output format (default): tab-separated lines like
 
 9_10#true    1#true    11#true    13#true    15#true    0
 
-(underscore-spearated token IDs for each target; last column is sentence ID)
+(underscore-separated token IDs for each target; last column is sentence ID)
 
 Pass -j to produce JSON output.
-
-We observe a couple of differences from SEMAFOR's RoteSegmenter:
-1) In SEMAFOR 3.0, lemmas were not lowercased before being looked up in the whitelist, 
-   so some additional targets are predicted here. This is now fixed in the Java codebase.
-2) The Java WordNet library (JWNL) performs aggressive simplification of words 
-   during lemmatization, for example stripping out prefixes/suffixes separated by a 
-   hyphen or period until it finds a dictionary match. More of those lemmas will be 
-   found in the whitelist than when using NLTK's morphy, which only removes 
-   inflectional morphology. (For instance, "scud-B missile" matches whitelist entry 
-   "scud_N missle_N" with the Java implementation, whereas here only "missle" is 
-   marked as a target.) Note that we are using the same whitelist file, which was 
-   presumably created with the JWNL lemmatization.
 
 @author: Nathan Schneider (nschneid@cs.cmu.edu)
 @since: 2013-05-25
 """
 
 from __future__ import print_function, division
-import sys, os, codecs, json
+import codecs
 from collections import Counter
+import json
+import os
+import sys
 
 from semafor.formats.malt_to_conll import read_conll
 from semafor.targetid.target_scanner import build_target_dicts
 
-def get_coarse_pos(pos):
-    return pos[0]   # this seems to be what was in the whitelist
 
-TARGETDICT_PATH = os.path.dirname(os.path.abspath(__file__))+'/target_dict.train.txt'
-UNIDICT_PATH = os.path.dirname(os.path.abspath(__file__))+'/unigram_dict.train.txt'
+TARGETDICT_PATH = os.path.dirname(os.path.abspath(__file__)) + '/target_dict.train.txt'
+UNIDICT_PATH = os.path.dirname(os.path.abspath(__file__)) + '/unigram_dict.train.txt'
 if not os.path.isfile(TARGETDICT_PATH) or not os.path.isfile(UNIDICT_PATH):
     build_target_dicts(TARGETDICT_PATH, UNIDICT_PATH)
 
@@ -60,9 +50,6 @@ with codecs.open(UNIDICT_PATH, 'r', 'utf-8') as unidictF:
         w, freq = ln[:-1].split('\t')
         UNIDICT[w] = int(freq)    # relative frequency
         POSDICT[w[-1]] += int(freq)
-
-#print(POSDICT, file=sys.stderr)
-#print(POSTARGETDICT, file=sys.stderr)
 
 MAX_LEN = 4 # the maximum length of ngrams we'll look for
 
@@ -102,55 +89,61 @@ DIR_PREPS = frozenset('into through to'.split())
 FORBIDDEN_POS_PREFIXES = frozenset(["PR", "CC", "IN", "TO"])
 
 
+def get_coarse_pos(pos):
+    return pos[0]   # this seems to be what was in the whitelist
+
+
 def get_segmentation(sentence):
     numTokens = len(sentence)
-    
+
     # start indices that we haven't used yet
     remainingStartIndices = set(range(numTokens))
-    
+
     lemmas = [token.lemma + "_" + get_coarse_pos(token.postag.upper()) for token in sentence]
 
     # look for ngrams, backing off to smaller n
     for n in range(MAX_LEN, 0, -1):
-        for start in range(numTokens-n+1):
+        for start in range(numTokens - n + 1):
             if start not in remainingStartIndices:
                 continue
             end = start + n
             ngramSpan = slice(start, end)
             ngramLemmas = ' '.join(lemmas[start:end])
-            if n==1 or ngramLemmas in TARGETDICT:
+            if n == 1 or ngramLemmas in TARGETDICT:
                 # found a good ngram, add it to results, and remove it from startIndices so we don't overlap later
                 candidate_target = sentence[ngramSpan]
                 #if shouldIncludeToken(candidate_target, sentence):
                 yes = False
 
-                if n>1 and TARGETDICT[ngramLemmas]>=1:
+                if n > 1 and TARGETDICT[ngramLemmas] >= 1:
                     yes = True
-                elif n==1 and (TARGETDICT[ngramLemmas]>=5 or UNIDICT[ngramLemmas]>=10):
+                elif n == 1 and (TARGETDICT[ngramLemmas] >= 5 or UNIDICT[ngramLemmas] >= 10):
                     # for unigrams, decide based on TARGETDICT vs. UNIDICT counts
-                    if (TARGETDICT[ngramLemmas]/(UNIDICT[ngramLemmas] or 1) >= 0.5 and not sentence[start].postag.upper().startswith('NNP')):
+                    if ((TARGETDICT[ngramLemmas] / (UNIDICT[ngramLemmas] or 1) >= 0.5 and
+                            not sentence[start].postag.upper().startswith('NNP'))):
                         yes = True
-                elif n==1:
+                elif n == 1:
                     # decide based on POSTARGETDICT vs. POSDICT counts
                     pos = sentence[start].postag.upper()
-                    if not pos.startswith('NNP'): # not a proper noun
+                    if not pos.startswith('NNP'):  # not a proper noun
                         cpos = ngramLemmas[-1]
-                        if cpos in POSDICT and POSTARGETDICT[cpos]/POSDICT[cpos] >= 0.5:
+                        if cpos in POSDICT and POSTARGETDICT[cpos] / POSDICT[cpos] >= 0.5:
                             yes = True
-                
+
                 # special cases
-                
+
                 # auxiliaries
-                if n==1 and ngramLemmas[:-1]=="will_":
-                    yes = (ngramLemmas!='will_M')
+                if n == 1 and ngramLemmas[:-1] == "will_":
+                    yes = (ngramLemmas != 'will_M')
                 #elif n==1 and sentence[start].postag.upper()=='MD':
                 #    yes = True  # modals other than 'will'
                 #    # including these modals hurts more than it helps, probably due to inconsistent annotations
-                elif n==1 and ngramLemmas[:-1]=="have_":
+                elif n == 1 and ngramLemmas[:-1] == "have_":
                     #if any(1 for t in sentence if t.lemma=='have') and any(1 for t in sentence if t.lemma=='question'):
                     #    assert False,sentence
-                    yes = any(1 for t in sentence if t.head==start+1 and t.deprel.upper() in ('OBJ','DOBJ')) # 'have' is a target iff it has an object
-                #elif n==1 and ngramLemmas[:-1]=="of_":
+                    yes = any(1 for t in sentence if t.head == start + 1 and t.deprel.upper() in (
+                        'OBJ', 'DOBJ')) # 'have' is a target iff it has an object
+                    #elif n==1 and ngramLemmas[:-1]=="of_":
                 #    prevToken, nextToken = sentence[start-1], sentence[start+1]
                 #    if prevToken.lemma in PRECEDING_WORDS_OF: yes = True
                 #    elif nextToken.form.lower() in FOLLOWING_WORDS_OF: yes = True
@@ -160,19 +153,18 @@ def get_segmentation(sentence):
                 #    else: yes = False
                 #    #return nextToken.ne.startswith("GPE") or nextToken.ne.startswith("LOCATION") or nextToken.ne.startswith("CARDINAL")
                 #    # including certain 'of' uses hurts more than it helps, probably due to inconsistent annotations
-                
-                
+
+
                 if yes:
                     # further filtering
-                    if n==1:
-                        if tuple(map(lambda x: x.lemma, sentence[start:start+2])) in [('of','course'),('in','particular')]:
+                    if n == 1:
+                        if tuple(map(lambda x: x.lemma, sentence[start:start + 2])) in [('of', 'course'),
+                                                                                        ('in', 'particular')]:
                             yes = False
-                        if tuple(map(lambda x: x.lemma, sentence[start-1:start+1])) in [('of','course'),('in','particular')]:
+                        if tuple(map(lambda x: x.lemma, sentence[start - 1:start + 1])) in [('of', 'course'),
+                                                                                            ('in', 'particular')]:
                             yes = False
-                        
-                        
-                            
-                        
+
                 if yes:
                     yield candidate_target
                     remainingStartIndices -= set(range(ngramSpan.start, ngramSpan.stop))
@@ -183,74 +175,92 @@ def shouldIncludeToken(candidate_target_tokens, sentence):
     Determines whether an ngram should be kept as a target or discarded based on hand-built rules.
     """
     # always include ngrams, n > 1
-    if len(candidate_target_tokens)>1: return True
+    if len(candidate_target_tokens) > 1:
+        return True
 
     token = candidate_target_tokens[0]
-    idx = token.id-1    # token ID is 1-based
+    idx = token.id - 1    # token ID is 1-based
     assert token is sentence[idx]
     # look up the word in pData
     form = token.form.lower()
     pos = token.postag
     lemma = token.lemma
     # look up the preceding and following words in pData, if they exist
-    prevToken = sentence[idx-1] if idx-1>=0 else None
-    nextToken = sentence[idx+1] if idx+1<len(sentence) else None
+    prevToken = sentence[idx - 1] if idx - 1 >= 0 else None
+    nextToken = sentence[idx + 1] if idx + 1 < len(sentence) else None
 
-    if form in FORBIDDEN_WORDS: return False
-    if form in LOC_PREPS: return False
-    if form in DIR_PREPS: return False
-    if form in TEMPORAL_PREPS: return False
-    if pos[:2] in FORBIDDEN_POS_PREFIXES: return False
+    if form in FORBIDDEN_WORDS:
+        return False
+    if form in LOC_PREPS:
+        return False
+    if form in DIR_PREPS:
+        return False
+    if form in TEMPORAL_PREPS:
+        return False
+    if pos[:2] in FORBIDDEN_POS_PREFIXES:
+        return False
     # skip "of course" and "in particular"
-    if prevToken and (prevToken.form.lower(), form) in [('of','course'), ('in','particular')]: return False
-    
-    if form=="of":
-        if prevToken.lemma in PRECEDING_WORDS_OF: return True
-        if nextToken.form.lower() in FOLLOWING_WORDS_OF: return True
+    if prevToken and (prevToken.form.lower(), form) in [('of', 'course'), ('in', 'particular')]:
+        return False
+
+    if form == "of":
+        if prevToken.lemma in PRECEDING_WORDS_OF:
+            return True
+        if nextToken.form.lower() in FOLLOWING_WORDS_OF:
+            return True
         if prevToken.postag[:2] in ("JJ", "CD"): return True
-        if nextToken.postag[:2]=="CD": return True
-        if nextToken.postag[:2]=="DT":
-            if idx+2<len(sentence):
-                nextNextToken = sentence[idx+2]
-                if nextNextToken.postag[:2]=="CD": return True
-        return nextToken.ne.startswith("GPE") or nextToken.ne.startswith("LOCATION") or nextToken.ne.startswith("CARDINAL")
-    
-    if form=="will": return pos!="MD"
-    if lemma=="have": return any(1 for t in sentence if t.head==token.id and t.deprel=='OBJ') # 'have' is a target iff it has an object
-    return lemma!="be"
+        if nextToken.postag[:2] == "CD":
+            return True
+        if nextToken.postag[:2] == "DT":
+            if idx + 2 < len(sentence):
+                nextNextToken = sentence[idx + 2]
+                if nextNextToken.postag[:2] == "CD":
+                    return True
+        return nextToken.ne.startswith("GPE") or nextToken.ne.startswith("LOCATION") or nextToken.ne.startswith(
+            "CARDINAL")
+
+    if form == "will":
+        return pos != "MD"
+    if lemma == "have":
+        # 'have' is a target iff it has an object
+        return any(1 for t in sentence if t.head == token.id and t.deprel == 'OBJ')
+    return lemma != "be"
+
 
 def main(fileP, output_format='legacy' or 'json'):
     with codecs.open(fileP, 'r', 'utf-8') as inF:
         for sentId, sentence in enumerate(read_conll(inF, lookup_lemmas=True)):
             assert None not in sentence
-            
+
             targets = list(get_segmentation(sentence))
-            
-            if output_format.lower()=='json':
-                sentJ = {"tokens": [tkn.form for tkn in sentence], 
+
+            if output_format.lower() == 'json':
+                sentJ = {"tokens": [tkn.form for tkn in sentence],
                          "frames": []}
                 for target in targets:
                     spansJ = []
                     for tkn in sorted(target, key=lambda x: x.id):
                         # group contiguous tokens into spans
-                        if spansJ and spansJ[-1]["end"]==tkn.id:
-                            spansJ["end"]+=1
-                            spansJ["text"]+= ' ' + tkn.form
+                        if spansJ and spansJ[-1]["end"] == tkn.id:
+                            spansJ[-1]["end"] += 1
+                            spansJ[-1]["text"] += ' ' + tkn.form
                         else:
-                            spansJ.append({"start": tkn.id-1, "end": tkn.id, "text": tkn.form})
+                            spansJ.append({"start": tkn.id - 1, "end": tkn.id, "text": tkn.form})
                     sentJ["frames"].append({"target": {"spans": spansJ}})
                 print(json.dumps(sentJ))
             else:
-                print('\t'.join('_'.join(str(token.id-1) for token in target_tokens)+'#true' for target_tokens in targets) + '\t' + str(sentId))
                 # output format: 9_10#true    1#true    11#true    13#true    15#true    0
-                # underscore-spearated token IDs for each target; last column is sentence ID
+                # underscore-separated token IDs for each target; last column is sentence ID
                 # (if no targets, will simply be a tab followed by sentence number)
+                print('\t'.join('_'.join(str(token.id - 1) for token in target_tokens) + '#true' for target_tokens in
+                                targets) + '\t' + str(sentId))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     args = sys.argv[1:]
     fmt = 'legacy'
-    if args[0]=='-j':
+    if args[0] == '-j':
         fmt = 'json'
         args.pop(0)
-    fileP, _ = args+[None]
+    fileP, _ = args + [None]
     main(fileP, output_format=fmt)
