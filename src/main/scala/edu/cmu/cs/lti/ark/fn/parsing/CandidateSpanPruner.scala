41ceef1c1652c29651714c24c3dbf1f5c6fb68c8
Sam Thomson <sammthomson@gmail.com>
2015-03-11 17:10:49 -0400

    CandidateSpanPruner wasn't finished. now it is.

--- src/main/scala/edu/cmu/cs/lti/ark/fn/parsing/CandidateSpanPruner.scala
+++ src/main/scala/edu/cmu/cs/lti/ark/fn/parsing/CandidateSpanPruner.scala
@@ -5,6 +5,7 @@ import java.util
 import edu.cmu.cs.lti.ark.fn.parsing.CandidateFrameElementFilters._
 import edu.cmu.cs.lti.ark.fn.parsing.CandidateSpanPruner.NON_BREAKING_LEFT_CONSTITUENT_POS
 import edu.cmu.cs.lti.ark.fn.utils.DataPointWithFrameElements
+import edu.cmu.cs.lti.ark.util.ds.Range0Based
 import edu.cmu.cs.lti.ark.util.nlp.parse.DependencyParse
 
 import scala.collection.JavaConverters._
@@ -18,53 +19,52 @@ class CandidateSpanPruner {
   /** Finds a set of candidate spans based on a dependency parse */
   def candidateSpansAndGoldSpans(dataPoint: DataPointWithFrameElements): util.List[DataPrep.SpanAndParseIdx] = {
     val candidates = candidateSpans(dataPoint.getParses.getBestParse)
-    val goldSpans = dataPoint.getOvertFrameElementFillerSpans.asScala.map(span => (span.start, span.end))
-    val result = for ((i, j) <- candidates ++ goldSpans) yield {
-      new DataPrep.SpanAndParseIdx(createSpanRange(i, j), 0)
+    val goldSpans = dataPoint.getOvertFrameElementFillerSpans.asScala
+    val result = for (range <- candidates.asScala ++ goldSpans) yield {
+      new DataPrep.SpanAndParseIdx(range, 0)
     }
-    result.toList.asJava
+    result.asJava
   }
 
   /**
    * Calculates an array of constituent spans based on the given dependency parse.
    * A constituent span is a token and all of its descendants.
-   * Adds the constituents to spanMatrix and heads
    *
    * @param depParse the dependency parse
    */
-  def candidateSpans(depParse: DependencyParse): Set[(Int, Int)] = {
-    val result = mutable.Set[(Int, Int)]((-1, -1)) // always include the empty span
+  def candidateSpans(depParse: DependencyParse): util.List[Range0Based] = {
+    val result = mutable.Set[Range0Based](CandidateFrameElementFilters.EMPTY_SPAN) // always include the empty span
     val nodes = depParse.getIndexSortedListOfNodes
     val length = nodes.length - 1
     // single tokens are always considered
-    result ++= (0 until length).map(i => (i, i))
+    result ++= (0 until length).map(i => new Range0Based(i, i))
     val subTrees = subTreeSpans(depParse)
     // full constituents
     result ++= subTrees
     // heuristics to try to recover finer-grained constituents when a node has multiple descendants
     for (i <- 0 until length) {
-      val (leftMost, rightMost) = subTrees(i)
+      val (leftMost, rightMost) = (subTrees(i).start, subTrees(i).end)
       if (leftMost < i && rightMost > i) {   // node has descendants on both sides
         val justLeft = i - 1
         if (
           justLeft >= 0 &&
-            result.contains((leftMost, justLeft)) &&
+            result.contains(new Range0Based(leftMost, justLeft)) &&
             !(justLeft == leftMost && NON_BREAKING_LEFT_CONSTITUENT_POS.contains(nodes(justLeft + 1).getPOS))
         ) {
           // include self and right children
-          result.add((i, rightMost))
+          result.add(new Range0Based(i, rightMost))
         }
         val justRight = i + 1
-        if (justRight <= length - 1 && result.contains((justRight, rightMost))) {
+        if (justRight <= length - 1 && result.contains(new Range0Based(justRight, rightMost))) {
           // include self and left children
-          result.add((leftMost, i))
+          result.add(new Range0Based(leftMost, i))
         }
       }
     }
-    result.toSet
+    result.toList.asJava
   }
 
-  def subTreeSpans(depParse: DependencyParse): Array[(Int, Int)] = {
+  def subTreeSpans(depParse: DependencyParse): Array[Range0Based] = {
     val nodes = depParse.getIndexSortedListOfNodes
     val length = nodes.length - 1
     // left[i] is the index of the left-most descendant of i
@@ -84,7 +84,7 @@ class CandidateSpanPruner {
         parentIdx = parents(parentIdx)
       }
     }
-    left zip right
+    (left zip right).map({ case (startIdx, endIdx) => new Range0Based(startIdx, endIdx) })
   }
 }
 object CandidateSpanPruner {
---
+++ src/test/scala/edu/cmu/cs/lti/ark/fn/parsing/CandidateSpanPrunerTest.scala
@@ -0,0 +1,43 @@
+package edu.cmu.cs.lti.ark.fn.parsing
+
+import edu.cmu.cs.lti.ark.fn.data.prep.formats.SentenceCodec._
+import edu.cmu.cs.lti.ark.fn.parsing.CandidateFrameElementFilters._
+import edu.cmu.cs.lti.ark.fn.parsing.DataPrep.SpanAndParseIdx
+import edu.cmu.cs.lti.ark.fn.utils.DataPointWithFrameElements
+import edu.cmu.cs.lti.ark.util.IntRanges._
+import org.scalatest.{Matchers, FlatSpec}
+import scala.collection.JavaConverters._
+
+/**
+ * @author sthomson@cs.cmu.edu
+ */
+class CandidateSpanPrunerTest extends FlatSpec with Matchers {
+  private val maltLine = "My/PRP$/2/NMOD kitchen/NN/5/SBJ no/RB/5/ADV longer/RB/3/AMOD smells/VBZ/0/ROOT ././5/P"
+  private val sentence = MaltCodec.decode(maltLine)
+  private val frameElementsLine = "0\t1.0\t1\tTemporal_collocation\tno.r\t2_3\tno longer\t0\n"
+  private val dataPointWithElements = new DataPointWithFrameElements(sentence, frameElementsLine)
+  private val pruner = new CandidateSpanPruner()
+  private val spanList = pruner.candidateSpans(dataPointWithElements.getParses.getBestParse).asScala.toList
+
+  "CandidateSpanPruner.candidateSpans" should "include the null span" in {
+    spanList should contain (SpanAndParseIdx.EMPTY_SPAN_AND_PARSE_IDX.span)
+  }
+
+  it should "include singletons" in {
+    import scala.collection.JavaConversions._
+    for (i <- xrange(sentence.size)) {
+      spanList should contain (createSpanRange(i, i))
+    }
+  }
+
+  it should "include constituents" in {
+    val expectedSpans = List(
+      createSpanRange(0, 1),
+      createSpanRange(2, 3),
+      createSpanRange(0, 5)
+    )
+    for (expectedSpan <- expectedSpans) {
+      spanList should contain (expectedSpan)
+    }
+  }
+}
